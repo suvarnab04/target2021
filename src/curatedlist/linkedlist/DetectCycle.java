66dcf262010d4b6748bcef4fb3da2e47b616f503
suvarnab04 <suvarnabothe@gmail.com>
2021-01-31 22:31:55 -0800

    head of cycle

--- src/curatedlist/linkedlist/DetectCycle.java
+++ src/curatedlist/linkedlist/DetectCycle.java
@@ -1,6 +1,12 @@
 package curatedlist.linkedlist;
+
+import java.util.List;
+
 /*
 https://leetcode.com/problems/linked-list-cycle/
+https://backtobackswe.com/platform/content/testing-for-cycles/video
+https://leetcode.com/problems/linked-list-cycle-ii/
+also check how to find head of the cycle
  */
 public class DetectCycle {
     public boolean hasCycle(ListNode head) {
@@ -18,5 +24,41 @@ public class DetectCycle {
             }
         }
         return false;
+    }
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow ==  fast){
+                slow = slow.next;
+                int count = 1;
+                while(slow!= fast){
+                    slow = slow.next;
+                    count++;
+                }
+                System.out.println("cycle length is::"+ count);
+                ListNode left = head;
+                ListNode right = head;
+                int i=0;
+                while(i< count){
+                    right = right.next;
+                    i++;
+                }
+                while(left!=right){
+                    left = left.next;
+                    right= right.next;
+
+                }
+                return left;
+            }
+        }
+        return null;
+
     }
 }
